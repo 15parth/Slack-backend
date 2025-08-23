466048088717d42a765615d24f0dca441c21d3c8
Parth Bhardwaj <pbhardwaj@datopic.com>
2025-08-23 16:28:47 +0530

    websockets initial setup

---
+++ src/router/v1/messagesRoute.js
@@ -0,0 +1,14 @@
+import express from 'express'
+
+import { getMessagesController } from '../../controllers/messageController.js'
+import { isAuthenticated } from '../../middlewares/authMiddleware.js'
+
+const router = express.Router()
+
+router.get(
+  '/messages/:channelId',
+  isAuthenticated,
+  getMessagesController
+)
+
+export default router
---
+++ src/service/messageService.js
@@ -0,0 +1,30 @@
+import { StatusCodes } from 'http-status-codes';
+
+import channelRepository from '../repositories/channelRepository.js'
+import messageRepository from '../repositories/messageRepository.js'
+import ClientError from '../utils/errors/clientError.js';
+import { isUserMemberOfWorkspace } from './workspaceService.js';
+
+export const getMessageService = async (messageParams, page, limit,user) => {
+
+  const channelDetails = await channelRepository.getChannelWithWorkspaceDetails(messageParams.channelId);
+
+  const  workspace = channelDetails.workspaceId;
+
+  const isMember = isUserMemberOfWorkspace(workspace,user)
+
+  if(!isMember){
+    throw new ClientError({
+      explanation:'User is not a member of this channel and can not get details',
+      message:'Invalid data send from client',
+      statusCode:StatusCodes.UNAUTHORIZED
+    })
+  }
+
+  const messages = await messageRepository.getPaginatedMessages(
+    messageParams,
+    page,
+    limit
+  )
+  return messages
+}
